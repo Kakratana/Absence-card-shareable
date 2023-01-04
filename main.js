fabbfb2fe95c3cf34a5de94622abd05be43ca85b
Kakratana <39192742+Kakratana@users.noreply.github.com>
2023-01-04 14:18:24 +0700

    cc

--- main.js
+++ main.js
@@ -135,7 +135,8 @@ download.addEventListener("click", function(){
 
 
 async function onShares() {
-        var x = document.getElementById("name").value;
+
+    var x = name.innerHTML;
 
         const note = 'សូមគោរព ជំរាបសួរលោកប្រធាន និងថ្នាក់ដឹកនាំ។ ខ្ញុំបាទ/នាងខ្ញុំ ' + x + ' សុំអនុញ្ញាតច្បាប់សម្រាកដូចទម្រង់ស្នើសុំខាងលើ សូមលោកប្រធាន និងថ្នាក់ដឹកនាំមេត្តាពិនិត្យឲ្យក្តីអនុគ្រោះ។';
         const dataUrl = canvas.toDataURL();
